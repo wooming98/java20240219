53221a24bb7a30b291fb58215c0fb5e0fe6d7a37
anwoomin <askcw39@naver.com>
2024-02-19 12:41:14 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("e");
+        System.out.println("f");
+    }
+}
