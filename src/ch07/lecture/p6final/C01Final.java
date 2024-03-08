d82b8a6cf601ae66d8a1bd932b27659aa07c78da
anwoomin <askcw39@naver.com>
2024-03-08 15:30:39 +0900

    final 메소드와 클래스

---
+++ src/ch07/lecture/p6final/C01Final.java
@@ -0,0 +1,19 @@
+package ch07.lecture.p6final;
+
+public class C01Final {
+
+}
+
+class C01Parent {
+    // final 메소드 : 하위 클래스가 재정의 하지 못하도록 함
+    final public void method1() {
+        System.out.println("C01Parent.method1");
+    }
+}
+
+class C01Child extends C01Parent {
+    // final 메소드는 재정의 안됨
+//    public void method1() {
+//        
+//    }
+}
---
+++ src/ch07/lecture/p6final/C02Final.java
@@ -0,0 +1,10 @@
+package ch07.lecture.p6final;
+
+public class C02Final {
+}
+
+// final class : 상속 못하게 함
+final class C02Parent { }
+
+// final class 상속 못함
+//class C02Child extends C02Parent { }
