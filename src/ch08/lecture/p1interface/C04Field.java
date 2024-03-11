0b2dc51d1b68aee5aa75aa0f973dc37af6df34c3
anwoomin <askcw39@naver.com>
2024-03-11 14:17:49 +0900

    인터페이스 filed

---
+++ src/ch08/lecture/p1interface/C04Field.java
@@ -0,0 +1,14 @@
+package ch08.lecture.p1interface;
+
+public class C04Field {
+
+}
+
+interface C04MyInterface {
+    // 인터페이스의 필드는 모두 public static final(생략되어있음)
+    int AGE = 10;
+    String NAME = "son hm";
+
+    // public abstract
+    void method();
+}
