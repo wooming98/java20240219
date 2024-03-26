295ef60102785d69055296e0bcd6b1bfdc24d312
anwoomin <askcw39@naver.com>
2024-03-26 12:36:37 +0900

    thread

---
+++ src/ch14/lecture/p1thread/C06Runnable.java
@@ -0,0 +1,21 @@
+package ch14.lecture.p1thread;
+
+public class C06Runnable {
+    public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0; i<30000; i++) {
+                    System.out.println(i);
+                }
+            }
+        });
+        t.start();
+
+        for(int i=0; i<30000; i++) {
+            System.err.println(i);
+        }
+    }
+}
+
+
---
+++ src/ch14/lecture/p1thread/C07Runnable.java
@@ -0,0 +1,14 @@
+package ch14.lecture.p1thread;
+
+public class C07Runnable {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+           for(int i=0; i<30000; i++) {
+               System.out.println(i);
+           }
+        });
+        for(int i=0; i<30000; i++) {
+            System.err.println(i);
+        }
+    }
+}
